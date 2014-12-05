350ec2a444142a91cd4f8a61e71ece8d66ad33d6
Jang <korea599293@naver.com>
2014-12-05 14:14:07 +0900

    lalalala

---
+++ jang.java
@@ -0,0 +1,9 @@
+package softtest;
+
+public class jang {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("왜거절할까?");
+		System.out.print("왜~????");
+	}
+}
